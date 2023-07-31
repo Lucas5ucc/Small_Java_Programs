cffed4f84c195da45d8167296aaba5281e81a875
Lucas Rocha <139708739+Lucas5ucc@users.noreply.github.com>
2023-07-31 10:14:31 +0100

    Add files via upload
    simple iterator

---
+++ integer-range/src/RangeTest.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+
+public class RangeTest {
+
+    public static void main(String[] args) {
+
+        Range r = new Range(-5, 15);
+
+        System.out.println("--- USING ITERATOR ---");
+        Iterator<Integer> it = r.iterator();
+
+        while (it.hasNext()) {
+
+            int i = it.next();
+
+            if (i == 1 || i == 2 || i == 3) {
+                it.remove();
+            }
+        }
+
+        for (Integer i : r) {
+            System.out.println("Iterated: " + i);
+        }
+
+    }
+
+}
